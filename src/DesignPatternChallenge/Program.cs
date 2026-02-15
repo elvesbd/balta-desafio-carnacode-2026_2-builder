f28d442ba9649b7aaac6e8e7387e9844f25c47db
elvesbd <elvesbd@yahoo.com>
2026-02-15 11:22:32 -0300

    feat: wire up Program.cs with builder demonstrations

--- src/DesignPatternChallenge/Program.cs
+++ src/DesignPatternChallenge/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DesignPatternChallenge;
+using DesignPatternChallenge.Builders;
+
+Console.WriteLine("=== Sistema de Relatórios (Builder Pattern) ===");
+
+Console.WriteLine("\n--- Director + PDF Builder ---");
+
+var pdfDirector = new ReportDirector(new PdfReportBuilder());
+var monthlyPdf = pdfDirector.BuildMonthlySalesReport(2024, 3);
+monthlyPdf.Generate();
+
+Console.WriteLine("\n--- Director + Excel Builder ---");
+
+var excelDirector = new ReportDirector(new ExcelReportBuilder());
+var quarterlyExcel = excelDirector.BuildQuarterlySalesReport(2024, 1);
+quarterlyExcel.Generate();
+
+Console.WriteLine("\n--- Director + HTML Builder ---");
+
+var htmlDirector = new ReportDirector(new HtmlReportBuilder());
+var annualHtml = htmlDirector.BuildAnnualSalesReport(2024);
+annualHtml.Generate();
